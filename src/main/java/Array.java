bfe332ffffeb9092db3629dd79ac1412f314643e
Evgenia <zhenja_a@bk.ru>
2021-03-26 17:56:24 +0300

    Initial commit

---
+++ src/main/java/Array.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Array {
+    public int[] getArrayAfterLast4(int[] arr){
+        
+    }
+
+    public boolean checkArrayConsistsOf1And4(int[] arr){
+ //       return true;
+    }
+}
